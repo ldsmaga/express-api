4aa117d4da98c20c0c156e42977648de9d977fae
Leandro Diniz <ldzsmaga@gmail.com>
2023-06-04 16:24:57 -0300

    MongoDB Atlas added

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const indexRoute = require('./routes/index');
+const userRoute = require('./routes/users');
+const url = 'mongodb+srv://ldzsmaga:Ft0CQL7sfUogQLGG@cluster-api.lo4s1ga.mongodb.net/?retryWrites=true&w=majority';
 
-const userRoute = require('./routes/User');
+mongoose.connect(url);
 
-app.use('/user', userRoute)
+mongoose.connection.on('error', err =>{
+    console.log('Erro na conexão:', err)
+});
+
+mongoose.connection.on('disconnected', () =>{
+    console.log('Aplicação desconectada do BD')
+});
+
+mongoose.connection.on('disconnected', () =>{
+    console.log('Aplicação desconectada do BD')
+});
+
+mongoose.connection.on('connected', () =>{
+    console.log('Aplicação conectada do BD')
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', indexRoute);
+app.use('/users', userRoute);
 
 app.listen(port, () => {
     console.log(`Application running on port http://localhost:${port}`)
-})
+});
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Users = require("../model/user");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find({});
+    return res.send(users);
+  } catch (err) {
+    return res.send({ error: err });
+  }
+});
+
+router.post("/create", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.send({ error: "Insufficient data" });
+
+  const query = Users.where({ email });
+  const user = await query.findOne().then((data) => {
+    if (data) return res.send({ error: "User already taken" });
+    
+    Users.create({ email, password }).then((data)=>{
+        return res.send(data)
+    });
+  });
+});
+
+router.put("/", (req, res) => {
+  res.send("User PUT request");
+});
+
+router.delete("/", function (req, res) {
+  res.send("Got a DELETE request at /user");
+});
+
+module.exports = router;
